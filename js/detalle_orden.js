b9873342cd9f31572e7de74a4eef9f3f143f6df8
Mardoqueo Carranza <jcarranza@gmail.com>
2017-03-08 14:15:38 -0600

    pantallas 2 y 3

---
+++ js/detalle_orden.js
@@ -0,0 +1,62 @@
+var app = {
+    SOME_CONSTANTS : false,  // some constant
+    // Application Constructor
+    initialize: function() {
+        console.log("console log init");
+        this.bindEvents();
+        this.initFastClick();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    initFastClick : function() {
+        window.addEventListener('load', function() {
+            FastClick.attach(document.body);
+        }, false);
+    },
+
+    // Phonegap is now ready...
+    onDeviceReady: function() {
+        console.log("device ready, start making you custom calls!");
+        orden = getParameter("vieworderId");
+        alert(orden);
+        $('#logoutButton').click(function(){
+        doLogout();
+      });
+
+          nombreUsuario = localStorage.getItem("nombre_usuario");
+          $('#divNombre').text(nombreUsuario);
+
+    }
+};
+  /* se dispara en lugar de onDeviceReady */
+  $(document).ready(function(){
+
+  var id_usuario = localStorage.getItem("id_usuario");
+
+  /* se obtienen las ordenes del usuario */
+/*
+  $.ajax({
+      type: 'POST',
+      url: webservices + 'getDetalleOrden',
+      data: {id: id_usuario},
+      dataType: 'json',
+      success: function (data) {
+
+        for (n in data){
+        //  console.log(  + " - " + data[n].direccion);
+          $('#listOrdenes').append("<div class='row'><div id='detalleItem'  class='col-xs-2' onclick='viewOrder("+ data[n].id_orden +")'><h4>" + data[n].id_orden + "</h4></div><div id='detalleItem'  class='col-xs-10'><h4>" + data[n].direccion + "</h4></div></div>");
+        }
+      }
+
+  });
+*/
+
+  var $loading = $('#loadingDiv').hide();
+  $(document).ajaxStart(function () {
+      $loading.show();
+    }).ajaxStop(function () {
+      $loading.hide();
+    });
+
+  });
