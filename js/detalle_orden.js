d4531d828358a7c33e842a6e288734153179eae9
Mardoqueo Carranza <jcarranza@gmail.com>
2017-03-13 14:08:30 -0600

    Envios de 13 marzo 2

--- js/detalle_orden.js
+++ js/detalle_orden.js
@@ -192,7 +192,6 @@ var app = {
 
 
 
-
   var $loading = $('#loadingDiv').hide();
   $(document).ajaxStart(function () {
       $loading.show();
@@ -205,8 +204,46 @@ var app = {
   });
 
 
-
       function verMandado(id){
         setParameter("verMandadoId",id);
         window.location = "detalle_mandado.html";
       }
+
+      function getImage() {
+         // Retrieve image file location from specified source
+         navigator.camera.getPicture(uploadPhoto, function(message) {
+          alert('get picture failed');
+       },{
+         quality: 50,destinationType: navigator.camera.DestinationType.FILE_URI, sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+       }
+               );
+
+           }
+
+     function uploadPhoto(imageURI) {
+         var options = new FileUploadOptions();
+         options.fileKey="file";
+         options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+         options.mimeType="image/jpeg";
+
+         var params = new Object();
+         params.value1 = "test";
+         params.value2 = "param";
+
+         options.params = params;
+         options.chunkedMode = false;
+
+         var ft = new FileTransfer();
+         ft.upload(imageURI,   webservices + "api/upload.php", win, thefail, options);
+     }
+
+     function win(r) {
+         console.log("Code = " + r.responseCode);
+         console.log("Response = " + r.response);
+         console.log("Sent = " + r.bytesSent);
+         alert(r.response);
+     }
+
+     function thefail(){
+       alert("Error al subir archivos");
+     }
